8ced6ae120d20e6de87f63fa9f74d4c2becd469c
Calvin <calvin.a.cooper@gmail.com>
2013-07-08 15:52:09 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Battleweb::Application.config.secret_key_base = '59a6d9a113b4b26362f3bcf278d16e1b46599165fec852040c1715f73416863b4f6d127adee30b8bef8d27db71e6c7ac25a4102375cb290b750e95fea348f66b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Battleweb::Application.config.session_store :cookie_store, key: '_battleweb_session'
